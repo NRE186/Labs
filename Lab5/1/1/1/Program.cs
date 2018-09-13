a76223c9bf53a41ddfd433659c8c80d2edd0d326
Web_Developer186 <TheTroller186@gmail.com>
2018-09-13 19:24:48 +0500

    1

---
+++ Lab5/1/1/1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 8, m = 7;
+            int tmp;
+            int[,] mas = new int[8, 7];
+            Random rand = new Random();
+
+            Console.WriteLine("Первоначальный массив");
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    mas[i, j] = rand.Next(-20, 20);
+                }
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Console.Write(mas[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Отсортированный массив");
+            for (var i = 0; i < mas.GetLength(0); i++)
+                for (var j = 0; j < mas.GetLength(1); j++)
+                    for (var k = 0; k < mas.GetLength(1); k++)
+                    {
+                        if (mas[i, j] >= mas[i, k]) continue;
+                        var temp = mas[i, j];
+                        mas[i, j] = mas[i, k];
+                        mas[i, k] = temp;
+                    }
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Console.Write(mas[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
