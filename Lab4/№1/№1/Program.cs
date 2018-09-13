62a01a0da20d9bb5a77e85b2204297be552855cd
Web_Developer186 <TheTroller186@gmail.com>
2018-09-13 16:56:03 +0500

    1

---
+++ Lab4/№1/№1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 25, a = 1;
+            double x,s = 1;
+            for (int i = 1; i < 25; i++){
+                double b = i * i + 1;
+                x = a / b;
+                s += x;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
