71d135b58bb07c9e5945366e64976e1a95c052a9
Web_Developer186 <TheTroller186@gmail.com>
2018-09-14 15:00:47 +0500

    1

--- Lab5/3/3/Program.cs
+++ Lab5/3/3/Program.cs
@@ -1,38 +1,39 @@
 ﻿using System;
 
-namespace _2
+namespace _3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n = 10,x = 0;
-            int[,] mas = new int[n, n];
+            int res = 0;
+            int[,] mas = new int[4, 5];
             Random rnd = new Random();
 
+            int x = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Первоначальный массив");
 
             for (int i = 0; i < mas.GetLength(0); i++)
             {
-                for (int j = 0; j < mas.GetLength(0); j++)
+                for (int j = 0; j < mas.GetLength(1); j++)
                 {
-                    mas[i, j] = rnd.Next(-50, 50);
+                    mas[i, j] = rnd.Next(0, 100);
                 }
             }
             for (int i = 0; i < mas.GetLength(0); i++)
             {
-                for (int j = 0; j < mas.GetLength(0); j++)
+                for (int j = 0; j < mas.GetLength(1); j++)
                 {
-                    if(mas[i,j] < 0)
+                    if (mas[i, j] < x && mas[i,j] % 2 == 0)
                     {
-                        x++;
+                        res++;
                     }
                     Console.Write(mas[i, j] + " ");
                 }
                 Console.WriteLine();
             }
             Console.WriteLine();
-            Console.WriteLine("Колличество отрицательных элементов - " + x);
+            Console.WriteLine("Колличество элементов меньших " + x + "и кратных двум - " + res);
             Console.ReadKey();
         }
     }
---
+++ Lab6/1/1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static long  Divd(long  N, long  start)
+        {
+            for ( long i = start; i * i <= N; i += 2)
+            {
+                if (N % i == 0) return i;
+            }
+            return N;
+        }
+        static void Main(string[] args)
+        {
+            long N = 600851475143;
+            long start = 3;
+
+            while (N > 1)
+            {
+                start = Divd(N, start);
+                N /= start;
+            }
+            Console.WriteLine(start);
+            Console.ReadKey();
+        }
+    }
+}
