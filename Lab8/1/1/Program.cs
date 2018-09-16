c9bb23593c2a3bac554a8cdbf20add3441cfbdcf
Web_Developer186 <TheTroller186@gmail.com>
2018-09-16 22:56:04 +0500

    1

---
+++ Lab8/1/1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"W:\C#\Labs\Labs\Lab8\file.txt";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line.Contains("1") || line.Contains("2") || line.Contains("3") || line.Contains("4") || line.Contains("5") || line.Contains("6")
+                            || line.Contains("7") || line.Contains("8") || line.Contains("9") || line.Contains("0"))
+                        {
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab8/3/3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"W:\C#\Labs\Labs\Lab8\file.txt";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    List<string> sLine = new List<string>();
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] split = line.Split(new Char[] { ' ', ',', '.', ':', '\t' });
+                        if (split[0].Length == 1)
+                        {
+                            Console.WriteLine(line);
+                        }
+                        if (split[0].Length != 1)
+                        {
+                            sLine.Add(line);
+                        }
+                    }
+                    foreach (string s in sLine)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
