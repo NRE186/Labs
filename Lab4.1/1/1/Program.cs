b618ffaf7f2e7e22ad9a97f11ba3f89086fd2edf
Web_Developer186 <TheTroller186@gmail.com>
2018-10-14 11:04:47 +0500

    1

---
+++ Lab4.1/1/1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(ReadLine());
+            int b = Int32.Parse(ReadLine());
+            int res = a * b;
+            int x = 0b00010000;
+            WriteLine(x);
+            ReadKey();
+        }
+    }
+}
