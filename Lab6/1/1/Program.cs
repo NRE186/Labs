32418315e07df5a0df5a0750fb9016353c2dc58d
Web_Developer186 <TheTroller186@gmail.com>
2018-10-24 12:29:27 +0500

    1

--- Lab6/1/1/Program.cs
+++ Lab6/1/1/Program.cs
@@ -4,26 +4,38 @@ namespace ConsoleApplication8
 {
     class Program
     {
-        static long  Divd(long  N, long  start)
+        static bool[] Prost(int n) // метод для нахождения простых чисел с помощью решета Эратосфена
         {
-            for ( long i = start; i * i <= N; i += 2)
+            bool[] m = new bool[n + 1];
+            for (int i = 2; i <= n; ++i)
             {
-                if (N % i == 0) return i;
+                if (m[i] == false)
+                {
+                    for (int j = 1; i + i * j <= n; ++j)
+                    {
+                        m[i + i * j] = true;
+                    }
+                }
             }
-            return N;
+            return m;
         }
         static void Main(string[] args)
         {
-            long N = 600851475143;
-            long start = 3;
-
-            while (N > 1)
             {
-                start = Divd(N, start);
-                N /= start;
+                int n = Convert.ToInt32(Math.Floor((Math.Sqrt(600851475143))));
+                bool[] m = Prost(n);
+                long ans = 2;
+                for (int i = n; i > 2; --i)
+                {
+                    if ((m[i] == false) && 600851475143 % i == 0)
+                    {
+                        ans = i;
+                        break;
+                    }
+                }
+                Console.WriteLine(ans);
+                Console.ReadKey();
             }
-            Console.WriteLine(start);
-            Console.ReadKey();
         }
     }
 }
