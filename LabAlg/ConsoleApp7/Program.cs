4c16cbf873c70d49a35f636447cfe131baf572c1
Web_Developer186 <TheTroller186@gmail.com>
2019-02-12 22:22:57 +0500

    Rewrite some code

--- LabAlg/ConsoleApp7/Program.cs
+++ LabAlg/ConsoleApp7/Program.cs
@@ -12,10 +12,9 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //Получение отрезков
-            double x1, x2, y1, y2;
+            double x1 = 0, x2 = 0, y1 = 0, y2 = 0;
             double d1 = -10;
             double d2 = 10;
-            double eps = 0.0001;
             double dx = 0.1;
             x1 = d1;
             x2 = x1 + dx;
@@ -25,17 +24,35 @@ namespace ConsoleApp1
                 y2 = F(x1);
                 if (y1 * y2 < 0)
                 {
-                    Console.WriteLine(string.Format("{0:0.00}", "[" + Math.Round((x1 - dx), 2) + ";" + Math.Round(x1,2) + "]"));
+                    break;
                 }
                 x1 = x2;
                 x2 = x1 + dx;
                 y1 = y2;
             }
             while (x1 < d2);
-
+            x2 = x1 - dx;
+            Console.WriteLine("Отрезок");
+            Console.WriteLine("[" + Math.Round(x2, 2) + ";" + Math.Round(x1, 2) + "]");
             //Метод бисекции
+            double eps = 0.000001;
+            double c = 0;
+            double x = 0;
+            double fc = 0;
+            double err = c;
 
-
+            while (Math.Abs(x1 - x2) > eps)
+            {
+                c = x1 + 0.5 * (x2 - x1);
+                fc = F(c);
+                if (Math.Abs(fc) < eps) break;
+                if (F(x1) * F(c) < 0)
+                    x2 = c;
+                else
+                    x1 = c;
+            }
+            Console.WriteLine("Метод бисекции ");
+            Console.WriteLine(Math.Round(c,7));
             Console.ReadKey();
         }
         static double F(double x)
